729e0afaa2055f94333a16e682df3513f3eb1968
Oladosu Abayomi <oladosuabayomi6@gmail.com>
2025-05-04 22:58:59 +0100

    Started implementation of project functionalites

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-"Use strict";
+"use strict";
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+const userInputEl = document.getElementById("user-input-el");
+const convertBtnEl = document.getElementById("convert-btn-el");
+const outputResultLength = document.getElementById("output-result-length");
+const outputResultVolume = document.getElementById("output-result-volume");
+const outputResultMass = document.getElementById("output-result-mass");
+
+function lengthConverter(unit) {
+  let meterToFeet = unit * 3.281;
+  return meterToFeet;
+}
+
+console.log(lengthConverter(20));
+
+function renderUnit(value, type1, type2) {
+  let unitToRender = `
+  ${value} ${type1} = ${lengthConverter(
+    value
+  )} ${type2}  | ${value} ${type2} = ${lengthConverter(value)} ${type1}
+`;
+
+  outputResultLength.innerHTML = unitToRender;
+}
+
+// get user input on btn clicked
+convertBtnEl.addEventListener("click", function () {
+  let userInput = "";
+  userInput = Number(userInputEl.value);
+
+  renderUnit(userInput, "meters", "feet");
+});
+
+// on click on "convert-btn" use input to make conversion in:
+// length,
+// volume,
+// mass
+// display result on the page
